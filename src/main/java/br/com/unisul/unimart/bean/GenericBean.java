a7c2d72728e673cf6ba6a1b42fec0b3e1ef8560e
Guilherme Nunes Rodrigues <guigonunes@gmail.com>
2017-06-06 19:52:02 -0300

    genericBean

---
+++ src/main/java/br/com/unisul/unimart/bean/GenericBean.java
@@ -0,0 +1,34 @@
+package br.com.unisul.unimart.bean;
+
+import java.io.Serializable;
+import java.util.Base64;
+
+import org.primefaces.model.UploadedFile;
+
+@SuppressWarnings("serial")
+public abstract class GenericBean implements Serializable {
+	
+	protected UploadedFile file;
+	
+	// Gets e Sets
+    
+    public String getImagemBase64(byte[] imagem) {
+    	if (imagem == null) return null;
+		return "data:image/jpg;base64," + Base64.getMimeEncoder().encodeToString(imagem);
+	}
+    
+    public UploadedFile getFile() {
+        return file;
+    }
+ 
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+    
+    // Metodos
+    
+    public void novo() {
+    	file = null;
+    }
+	
+}
--- src/main/java/br/com/unisul/unimart/bean/produtoCategoriaBean.java
+++ src/main/java/br/com/unisul/unimart/bean/produtoCategoriaBean.java
@@ -2,9 +2,7 @@ package br.com.unisul.unimart.bean;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.Serializable;
 import java.nio.file.Files;
-import java.util.Base64;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -13,25 +11,17 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 
 import org.omnifaces.util.Messages;
-import org.primefaces.model.UploadedFile;
 
 import br.com.unisul.unimart.dao.ProdutoCategoriaDao;
 import br.com.unisul.unimart.domain.ProdutoCategoria;
 
-
-
-
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
-public class produtoCategoriaBean implements Serializable {
+public class produtoCategoriaBean extends GenericBean {
 	
 	private ProdutoCategoria produtoCategoria;
 	private	List<ProdutoCategoria> produtoCategoriaList;
-	private UploadedFile file;
-	
-	@SuppressWarnings("unused")
-	private String imagemBase64;
 	
 	public ProdutoCategoria getProdutoCategoria() {
 		return produtoCategoria;
@@ -49,23 +39,6 @@ public class produtoCategoriaBean implements Serializable {
 		this.produtoCategoriaList = ProdutoCategoriaList;
 	}
 	
-	public UploadedFile getFile() {
-        return file;
-    }
- 
-    public void setFile(UploadedFile file) {
-        this.file = file;
-    }
-    
-    public String getImagemBase64(byte[] imagem) {
-    	if (imagem == null) return null;
-		return "data:image/jpg;base64," + Base64.getMimeEncoder().encodeToString(imagem);
-	}
-    
-    public void setImagemBase64(String imagemBase64) {
-		this.imagemBase64 = imagemBase64;
-	}
-	
 	@PostConstruct
 	public void listar() {
 		try {
@@ -81,7 +54,6 @@ public class produtoCategoriaBean implements Serializable {
 	public void novo() {
 		produtoCategoria = new ProdutoCategoria();
 		file = null;
-		imagemBase64 = null;
 	}
 	
 	public void excluir(ActionEvent event) {
@@ -103,20 +75,7 @@ public class produtoCategoriaBean implements Serializable {
 	
 	public void salvar() {
 		try {
-			if (!file.getFileName().equals("")) {
-			    //--colocar imagem no produtoCategoria
-			    produtoCategoria.setImagem(file.getContents());
-			} else {
-				//--se não tiver imagem selecionada cadastra uma imagem padrao
-				//--Alterar para pegar caminho relativo
-				//--Atualizaa
-				File f = new File("C:\\Users\\heto1\\git\\Unimart\\src\\img\\SemImagem.png");
-	    		try {
-					produtoCategoria.setImagem(Files.readAllBytes(f.toPath()));
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
+			defineImagem();
 			ProdutoCategoriaDao produtoCategoriaDao = new ProdutoCategoriaDao();
 			produtoCategoriaDao.merge(produtoCategoria);
 			Messages.addGlobalInfo("Categoria de produto cadastrado com sucesso");
@@ -125,6 +84,23 @@ public class produtoCategoriaBean implements Serializable {
 		} catch (Exception e) {
 			Messages.addGlobalError("Erro ao cadastrar categoria de produto");
 			e.printStackTrace();
+		}
+	}
+
+	private void defineImagem() {
+		if (!file.getFileName().equals("")) {
+		    //--colocar imagem no produtoCategoria
+		    produtoCategoria.setImagem(file.getContents());
+		} else {
+			//--se não tiver imagem selecionada cadastra uma imagem padrao
+			//--Alterar para pegar caminho relativo
+			//--Atualizaa
+			File f = new File("C:\\Users\\heto1\\git\\Unimart\\src\\img\\SemImagem.png");
+			try {
+				produtoCategoria.setImagem(Files.readAllBytes(f.toPath()));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
